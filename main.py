579ab2e291fc9c49f412edd40e7c6174fe9980be
Arthur Kozhemyakin <trable228@icloud.com>
2025-07-09 18:56:01 +0300

    delete temp files in every condition after action

--- main.py
+++ main.py
@@ -6,18 +6,25 @@ from zip_postprocessor import rename_images_to_match_html, prepare_upload_folder
 
 
 def main():
+    images_dir = None
+    compressed_path = None
+    converted_path = None
+    upload_folder_path = None
+
     try:
         word_path = select_word_file()
         images_dir = extract_images_from_docx(word_path)
         compressed_path = compress_images_in_docx(word_path)
-
         converted_path = convert(compressed_path)
+
         rename_images_to_match_html(
             images_dir_path=images_dir,
             converted_zip_path=converted_path
         )
 
-        html_path, upload_zip_path, upload_folder_path = prepare_upload_folder(converted_path, images_dir, word_path)
+        html_path, upload_zip_path, upload_folder_path = prepare_upload_folder(
+            converted_path, images_dir, word_path
+        )
 
         process_upload_flow(
             html_path=html_path,
@@ -25,14 +32,12 @@ def main():
             original_zip_path=converted_path
         )
 
-        print()
-        delete_path(images_dir)
-        delete_path(compressed_path)
-        delete_path(converted_path)
-        delete_path(upload_folder_path)
-
     except Exception as e:
         print(f"🔴 Критическая ошибка: {e}")
+
+    finally:
+        for path in [images_dir, compressed_path, converted_path, upload_folder_path]:
+            delete_path(path)
 
 
 if __name__ == '__main__':
