0f73cedff9494b320285a43af8c1176796d44c75
Arthur Kozhemyakin <trable228@icloud.com>
2025-07-09 15:34:40 +0300

    use constants for diff domens

--- constants.py
+++ constants.py
@@ -1,8 +1,10 @@
 # config.py
 
-BASE_URL = "http://localhost:8000/"
+DEBUG_SERVER = "http://127.0.0.1:8000/"
+TEST_SERVER = "https://schedule-app-test.dokka2.duckdns.org/"
+PROD_SERVER = "https://schedule.kodamaclass.com/"
 
-MATERIALS_API = BASE_URL + "materials/api/"
+MATERIALS_API = DEBUG_SERVER + "materials/api/"
 
 API_COURSES_IDS = MATERIALS_API + "courses-with-levels/"
 API_GUIDES_ORDER = MATERIALS_API + "level-guides/"
